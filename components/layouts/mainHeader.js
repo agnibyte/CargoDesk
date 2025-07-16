b71671082180fd03d9c11ec7d2accd701ce4ed82
Suraj <surajsangle00@gmail.com>
2025-07-16 21:05:19 +0530

    bold

--- components/layouts/mainHeader.js
+++ components/layouts/mainHeader.js
@@ -228,7 +228,7 @@ export default function MainHeader({ pageData = {} }) {
               <Link
                 key={idx}
                 href={item.url}
-                className={`px-4 py-2  font-bold rounded-lg transition-colors ${
+                className={`px-4 py-2 rounded-lg transition-colors ${
                   router.pathname === item.url
                     ? "text-blue-600"
                     : "text-gray-700"
@@ -242,7 +242,7 @@ export default function MainHeader({ pageData = {} }) {
                 key={idx}
               >
                 <button
-                  className="px-4 py-2 font-bold text-gray-700 rounded-lg transition-colors  flex items-center gap-2"
+                  className="px-4 py-2 text-gray-700 rounded-lg transition-colors  flex items-center gap-2"
                   aria-haspopup="true"
                   aria-expanded={activeMenu === idx}
                 >
