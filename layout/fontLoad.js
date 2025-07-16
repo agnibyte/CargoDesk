04cb77b9fd754cef3d09eca4cad8e2b44c6402d1
Suraj <surajsangle00@gmail.com>
2025-07-16 22:23:47 +0530

    feat: add FontLoad component for Google Fonts integration and update global font family to Rubik

---
+++ layout/fontLoad.js
@@ -0,0 +1,21 @@
+const FontLoad = () => {
+  return (
+    <>
+      <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+};
+
+export default FontLoad;
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,10 @@
+import FontLoad from "@/layout/fontLoad";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <FontLoad />
       <Head />
       <body>
         <Main />
