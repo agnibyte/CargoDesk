497c76444f3598eb7f412489e4c022ca280d9536
Suraj <surajsangle00@gmail.com>
2025-07-26 16:43:51 +0530

    send button changes

--- components/message/messageWrapper.js
+++ components/message/messageWrapper.js
@@ -6,6 +6,8 @@ import { postApiData } from "@/utilities/services/apiService";
 import PrevMessageCard from "./prevMessageCard";
 import CommonModal from "../common/commonModal";
 import GoogleContacts from "./googleContacts";
+
+import { RiSendPlaneFill } from "react-icons/ri";
 import Link from "next/link";
 
 export default function MessageWrapper({ pageData, contacts = [] }) {
@@ -301,21 +303,7 @@ export default function MessageWrapper({ pageData, contacts = [] }) {
             >
               <div className="svg-wrapper-1">
                 <div className="svg-wrapper">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                  >
-                    <path
-                      fill="none"
-                      d="M0 0h24v24H0z"
-                    />
-                    <path
-                      fill="currentColor"
-                      d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                    />
-                  </svg>
+                  <RiSendPlaneFill />
                 </div>
               </div>
               <span>{loading ? "Sending..." : "Send"}</span>
@@ -329,21 +317,7 @@ export default function MessageWrapper({ pageData, contacts = [] }) {
           >
             <div className="svg-wrapper-1">
               <div className="svg-wrapper">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    fill="none"
-                    d="M0 0h24v24H0z"
-                  />
-                  <path
-                    fill="currentColor"
-                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                  />
-                </svg>
+                <RiSendPlaneFill />
               </div>
             </div>
             <span>{loading ? "Sending..." : "Send"}</span>
