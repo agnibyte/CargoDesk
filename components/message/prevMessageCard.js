fed904cc0b9d0a5135f7b8fc95caf01a4fdc7693
Suraj <surajsangle00@gmail.com>
2025-08-01 21:17:23 +0530

    css height sert

--- components/message/prevMessageCard.js
+++ components/message/prevMessageCard.js
@@ -11,7 +11,7 @@ export default function PrevMessageCard({
   handleCopy,
 }) {
   return (
-    <div className="relative w-full lg:w-[48%] bg-white shadow-md rounded-lg p-4 border border-gray-200">
+    <div className="relative w-full lg:w-[48%] bg-white shadow-md rounded-lg p-4 border border-gray-200 h-min">
       {/* Buttons */}
       <div className="absolute top-2 right-2 flex gap-2">
         <div
