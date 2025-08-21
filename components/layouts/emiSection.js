c4ab379050a72eda155a31e5e5444b005be89cc9
Suraj <surajsangle00@gmail.com>
2025-08-21 23:12:23 +0530

    emai form used

--- components/layouts/emiSection.js
+++ components/layouts/emiSection.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
+import EmiForm from "../emi/emiForm";
 export default function EmiSection() {
   return (
     <>
       <h1 className="text-2xl font-bold mb-4">EMI and Truck Details</h1>
+      <EmiForm />
     </>
   );
 }
