65653f78e1463aaa3e2f039728a6467d37da4bf7
Suraj <surajsangle00@gmail.com>
2025-08-21 22:55:54 +0530

    delete imorts

--- components/layouts/emiSection.js
+++ components/layouts/emiSection.js
@@ -1,54 +1,5 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import {
-  Button,
-  TextField,
-  Grid,
-  Typography,
-  Box,
-  Card,
-  CardContent,
-  Select,
-  MenuItem,
-  InputLabel,
-  FormControl,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from "@mui/material";
-
-// Function to calculate EMI
-const calculateEmi = (loanAmount, interestRate, loanTenure) => {
-  const principal = parseFloat(loanAmount);
-  const rate = parseFloat(interestRate) / 100 / 12; // Monthly rate
-  const tenure = parseInt(loanTenure);
-
-  if (principal && rate && tenure) {
-    const emiValue =
-      (principal * rate * Math.pow(1 + rate, tenure)) /
-      (Math.pow(1 + rate, tenure) - 1);
-
-    const totalAmount = emiValue * tenure;
-    const interestPaid = totalAmount - principal;
-
-    return {
-      emi: emiValue.toFixed(2),
-      totalPayment: totalAmount.toFixed(2),
-      totalInterest: interestPaid.toFixed(2),
-    };
-  }
-
-  return {
-    emi: 0,
-    totalPayment: 0,
-    totalInterest: 0,
-  };
-};
-
 export default function EmiSection() {
   return (
     <>
