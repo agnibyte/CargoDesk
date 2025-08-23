d1b343f3b7b93339b3f2ed6b0393c5dc469abc3c
Suraj <surajsangle00@gmail.com>
2025-08-23 23:07:26 +0530

    api added

---
+++ pages/api/emi/add-new.js
@@ -0,0 +1,21 @@
+import { addNewContactController } from "@/backend/controllers/contactController";
+
+export default function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    const request = req.body;
+    const response = {
+      status: false,
+    };
+
+    addNewContactController(request)
+      .then((result) => {
+        res.status(200).json(result);
+        resolve(result);
+      })
+      .catch((error) => {
+        response.error = error;
+        res.status(200).json(response);
+        resolve();
+      });
+  });
+}
