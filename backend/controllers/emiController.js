2da259e810d6b7b440896c9dd162fff53b208b22
Suraj <surajsangle00@gmail.com>
2025-09-06 00:43:29 +0530

    format file controller

--- backend/controllers/emiController.js
+++ backend/controllers/emiController.js
@@ -1,100 +1,101 @@
 import { addNewEmiModel, getAllEmisModel } from "../models/emiModel";
 
 export function addNewEmiController(request) {
-    return new Promise((resolve, reject) => {
-        const response = {
-            status: false,
-        };
+  return new Promise((resolve, reject) => {
+    const response = {
+      status: false,
+    };
 
-        addNewEmiModel(request)
-            .then((result) => {
-                if (result.status) {
-                    response.status = true;
-                    response.message = result.message;
-                    resolve(response);
-                } else {
-                    response.message = result.message;
-                    resolve(response);
-                }
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
+    addNewEmiModel(request)
+      .then((result) => {
+        if (result.status) {
+          response.status = true;
+          response.message = result.message;
+
+          resolve(response);
+        } else {
+          response.message = result.message;
+          resolve(response);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 export function getAllEmiDataList(request) {
-    return new Promise((resolve, reject) => {
-        const response = {
-            status: false,
-        };
+  return new Promise((resolve, reject) => {
+    const response = {
+      status: false,
+    };
 
-        getAllEmisModel(request)
-            .then((result) => {
-                if (result.status) {
-                    response.status = true;
-                    response.message = result.message;
-                    response.data = result.data;
-                    resolve(response);
-                } else {
-                    response.message = result.message;
-                    resolve(response);
-                }
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
+    getAllEmisModel(request)
+      .then((result) => {
+        if (result.status) {
+          response.status = true;
+          response.message = result.message;
+          response.data = result.data;
+          resolve(response);
+        } else {
+          response.message = result.message;
+          resolve(response);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 // uodate emi entry
 export function updateEmiController(id, data) {
-    return new Promise((resolve, reject) => {
-        const response = {
-            status: false,
-        };
-        updateEmiModel(id, data)
-            .then((result) => {
-                if (result.status) {
-                    response.status = true;
-                    response.message = result.message;
-                    resolve(response);
-                } else {
-                    response.message = result.message;
-                    resolve(response);
-                }
-            })
-            .catch((error) => {
-                {
-                    console.error("Error updating EMI entry:", error);
-                    reject(error);
-                }
-            });
-    });
+  return new Promise((resolve, reject) => {
+    const response = {
+      status: false,
+    };
+    updateEmiModel(id, data)
+      .then((result) => {
+        if (result.status) {
+          response.status = true;
+          response.message = result.message;
+          resolve(response);
+        } else {
+          response.message = result.message;
+          resolve(response);
+        }
+      })
+      .catch((error) => {
+        {
+          console.error("Error updating EMI entry:", error);
+          reject(error);
+        }
+      });
+  });
 }
 
 // delete emi entry
 export function deleteEmiController(id) {
-    return new Promise((resolve, reject) => {
-        const response = {
-            status: false,
-        };
-        deleteEmiModel(id)
-            .then((result) => {
-                if (result.status) {
-                    response.status = true;
-                    response.message = result.message;
-                    resolve(response);
-                } else {
-                    response.message = result.message;
-                    resolve(response);
-                }
-            })
-            .catch((error) => {
-                {
-                    console.error("Error deleting EMI entry:", error);
-                    reject(error);
-                }
-            });
-    });
+  return new Promise((resolve, reject) => {
+    const response = {
+      status: false,
+    };
+    deleteEmiModel(id)
+      .then((result) => {
+        if (result.status) {
+          response.status = true;
+          response.message = result.message;
+          resolve(response);
+        } else {
+          response.message = result.message;
+          resolve(response);
+        }
+      })
+      .catch((error) => {
+        {
+          console.error("Error deleting EMI entry:", error);
+          reject(error);
+        }
+      });
+  });
 }
