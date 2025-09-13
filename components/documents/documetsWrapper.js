20f6ec7052208e6076ada60adfb480fe9f5a487c
Suraj <surajsangle00@gmail.com>
2025-09-13 23:06:22 +0530

    doument wrapper compone added

--- components/documents/documetsWrapper.js
+++ components/documents/documetsWrapper.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function DocumetsWrapper({ pageData }) {
-    console.log('pageData jjjj', pageData)
-  return <div>DocumetsWrapper</div>;
+  const [reminderModal, setReminderModal] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [updateLoading, setUpdateLoading] = useState(false);
+  const [addLoading, setAddLoading] = useState(false);
+  const [reminderData, setReminderData] = useState("");
+  const [documentTableData, setDocumentTableData] = useState([]);
+
+  const onClickAddReminder = () => {
+    setIsEdit(false);
+    setReminderModal(true);
+  };
+  console.log("pageData jjjj", pageData);
+  return (
+    <>
+      <DocumentsSection
+        setReminderData={setReminderData}
+        setReminderModal={setReminderModal}
+        setIsEdit={setIsEdit}
+        tableData={documentTableData}
+        setTableData={setDocumentTableData}
+        onClickAddDocument={onClickAddReminder}
+      />
+
+      <CommonModal
+        modalTitle={isEdit ? "Edit Document" : "Add New Document"}
+        modalOpen={reminderModal}
+        setModalOpen={setReminderModal}
+        modalSize={"w-11/12 md:w-3/6"}
+      >
+        <AddDocumentForm
+          setReminderModal={setReminderModal}
+          addReminderData={addReminderData}
+          reminderData={reminderData}
+          isEdit={isEdit}
+          setIsEdit={setIsEdit}
+          isLoading={isEdit ? updateLoading : addLoading}
+          updateReminderData={updateReminderData}
+        />
+      </CommonModal>
+    </>
+  );
 }
