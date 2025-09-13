d498b4f7a8505b2f2962eb6d9a7aee90b560d645
Suraj <surajsangle00@gmail.com>
2025-09-13 23:06:57 +0530

    required funaction added

--- components/documents/documetsWrapper.js
+++ components/documents/documetsWrapper.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import CommonModal from "../common/commonModal";
+import { postApiData } from "@/utilities/services/apiService";
 
 export default function DocumetsWrapper({ pageData }) {
   const [reminderModal, setReminderModal] = useState(false);
@@ -12,6 +14,55 @@ export default function DocumetsWrapper({ pageData }) {
     setIsEdit(false);
     setReminderModal(true);
   };
+
+  
+    const addReminderData = async (data) => {
+      const payload = {
+        vehicleNo: data.vehicleNo,
+        documentType: data.documentType,
+        expiryDate: data.expiryDate,
+        note: data.note,
+      };
+      setAddLoading(true);
+      try {
+        const response = await postApiData("ADD_NEW_VEHICALE_DOCUMENTS", payload);
+        if (response.status) {
+          const LatestData = [...documentTableData, { id: response.id, ...data }];
+          setDocumentTableData(LatestData);
+          setReminderModal(false);
+        }
+      } catch (error) {
+        console.error("Error occurred during form submission:", error);
+      }
+      setAddLoading(false);
+    };
+  
+    const updateReminderData = async (updatedData) => {
+      const payload = {
+        id: updatedData.id,
+        vehicleNo: updatedData.vehicleNo,
+        documentType: updatedData.documentType,
+        expiryDate: updatedData.expiryDate,
+        note: updatedData.note,
+      };
+      setUpdateLoading(true);
+      try {
+        const response = await postApiData("UPDATE_VEHICALE_DOCUMENTS", payload);
+        if (response.status) {
+          setDocumentTableData((prevData) =>
+            prevData.map((item) =>
+              item.id === updatedData.id ? { ...item, ...updatedData } : item
+            )
+          );
+        }
+        setReminderModal(false);
+        setIsEdit(false);
+      } catch (error) {
+        console.error("Error occurred during form submission:", error);
+      }
+      setUpdateLoading(false);
+    };
+  
   console.log("pageData jjjj", pageData);
   return (
     <>
