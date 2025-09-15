f387002260a559d6a983c4a33c01f58f78fbeeda
Suraj <surajsangle00@gmail.com>
2025-09-15 22:11:29 +0530

    log remove

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,6 @@ export async function getServerSideProps(context) {
 
   // Token is valid → fetch user details from DB
   const [userDetails] = await Promise.all([getUserDetailsById(decoded.userId)]);
-  console.log("userDetails", userDetails);
 
   if (!userDetails?.status || !userDetails.data) {
     // User not found → clear cookie & redirect
